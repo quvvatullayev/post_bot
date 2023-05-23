c2da74fdbf36affff0fe5716874dbdc689831665
ogabek <quvvatullayev@gmail.com>
2023-05-23 11:34:55 +0500

    add add message

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from telegram import Update
 from main import Post
 
@@ -11,6 +11,8 @@ post = Post()
 
 dispatcher.add_handler(CommandHandler('start', post.start))
 dispatcher.add_handler(MessageHandler(Filters.text('add channel'), post.get_text))
+dispatcher.add_handler(CallbackQueryHandler(post.add_message, pattern='Yes'))
+dispatcher.add_handler(CallbackQueryHandler(post.delet_message, pattern='No'))
 dispatcher.add_handler(MessageHandler(Filters.all, post.add_post_imge))
 
 updater.start_polling()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackContext
 from db import DB
 
@@ -8,7 +8,7 @@ class Post:
     def start(self, update: Update, context: CallbackContext) -> None:
         bot = context.bot
         chat_id = update.effective_chat.id
-        text  = "Hello, I'm a bot that can post messages to your channel.\n"
+        text  = "Hello, I'm a bot that can post messages to your channel✋.\n"
         text += "To get started, add me to your channel as an administrator and send me the channel name.\n"
 
         reply_markup = ReplyKeyboardMarkup(
@@ -23,6 +23,24 @@ class Post:
     def add_post_imge(self, update: Update, context: CallbackContext) -> None:
         bot = context.bot
         chat_id = update.effective_chat.id
+
+        keyboard = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(text='Yes🆗', callback_data=f'Yes_{update.message.message_id}'), 
+                    InlineKeyboardButton(text='No⛔️', callback_data='No')
+                ]
+            ]
+            )
+
+        bot.sendMessage(chat_id, 'Shall I send a message❓', reply_markup=keyboard)
+        
+    def add_message(self, update: Update, context: CallbackContext):
+        query = update.callback_query
+        bot = context.bot
+        chat_id = query.message.chat_id
+        message_id = query.data.split('_')[1]
+
         all_channels = db.get_all_channels()
 
         for channel in all_channels:
@@ -30,8 +48,18 @@ class Post:
             bot.forward_message(
                 chat_id=channel['name'], 
                 from_chat_id=chat_id, 
-                message_id=update.message.message_id
+                message_id=message_id,
+                disable_notification = True
                 )
+        text = "The message was sent successfully✅"
+        query.edit_message_text(text, reply_markup=None)
+
+    def delet_message(self, update: Update, context: CallbackContext):
+        query = update.callback_query
+
+        text = "The message was rejected🚫"
+        query.edit_message_text(text, reply_markup=None)
+
     def get_text(self, update: Update, context: CallbackContext):
         bot = context.bot
         chat_id = update.message.chat_id
