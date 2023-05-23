74b4f12cfce758fce8c7da96c97df02d84289f23
ogabek <quvvatullayev@gmail.com>
2023-05-23 11:53:27 +0500

    add channel db

--- db.py
+++ db.py
@@ -6,6 +6,7 @@ class DB:
         self.db = TinyDB(path, indent=4, separators=(',', ': '))
         self.query = Query() 
         self.channels = self.db.table('channels')
+        self.channel_add = self.db.table('channel_add')
 
     def add_channel(self, channel_id, channel_name):
         self.channels.insert(Document({'id': channel_id, 'name': channel_name}, doc_id=channel_id))
@@ -18,3 +19,12 @@ class DB:
     
     def delete_channel(self, channel_id):
         self.channels.remove(self.query.id == channel_id)
+
+    def add_channel_add(self, chat_id, channel_name):
+        self.channel_add.insert(Document({'id': chat_id, 'name': channel_name}, doc_id=chat_id))
+
+    def get_add_channel(self, chat_id):
+        return self.channel_add.get(self.query.id == chat_id)
+    
+    def delete_add_channel(self, chat_id):
+        self.channel_add.remove(self.query.id == chat_id)        
